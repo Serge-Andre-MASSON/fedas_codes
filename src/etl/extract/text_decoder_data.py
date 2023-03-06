ab99d81e0850b7dc49e5e249e753fc6ea6e0432c
Serge <serge.andre.masson@gmail.com>
2023-03-06 05:56:00 +0100

    Implement text_decoder_data extractor

---
+++ src/etl/extract/text_decoder_data.py
@@ -0,0 +1,22 @@
+from pandas import DataFrame, Series
+
+
+def get_text_decoder_data(df: DataFrame) -> Series:
+    """Extract correct fedas codes from source DataFrame and return
+    them as a pd.Series in which each element is a tokenizer-ready version
+    of the fedas code e.g. : ["<sos>", "1", "25", "46", "5", "<eos>"]
+
+    Returns:
+        _type_: pd.Series
+    """
+    return df["correct_fedas_code"].astype(str)\
+                                   .apply(split)\
+                                   .apply(add_special_tokens)
+
+
+def split(c: str) -> list[str]:
+    return [c[0], c[1:3], c[3:5], c[-1]]
+
+
+def add_special_tokens(l: list[str]) -> list[str]:
+    return ["<sos>"] + l + ["<eos>"]
---
+++ tests/etl/extract/test_text_decoder_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from etl.extract.text_decoder_data import (
+    split,
+    add_special_tokens
+)
+
+
+train_data_path = "data/train_technical_test.csv"
+df = pd.read_csv(train_data_path)
+
+
+def test_splits():
+    fedas_code = "125465"
+    splitted_fedas_code = ["1", "25", "46", "5"]
+    assert split(fedas_code) == splitted_fedas_code
+
+
+def test_add_special_tokens():
+    splitted_fedas_code = ["1", "25", "46", "5"]
+    assert add_special_tokens(splitted_fedas_code) == [
+        "<sos>", "1", "25", "46", "5", "<eos>"
+    ]
